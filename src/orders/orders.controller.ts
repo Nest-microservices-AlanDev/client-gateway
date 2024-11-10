7c1a4c70606fbcfb5531d568c154cfd7f4a799e1
Alan Gonzalez <alanfga1@gmail.com>
2024-11-09 21:46:57 -0700

    finish gateway

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,7 +10,7 @@ import {
   Patch,
 } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { OrderPaginationDto, StatusDto } from './dto';
@@ -18,18 +18,23 @@ import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(
-    @Inject(ORDER_SERVICE) private readonly orderClient: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderClient.send('createOrder', createOrderDto);
+    return this.client.send('createOrder', createOrderDto);
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.orderClient.send('findAllOrders', orderPaginationDto);
+  async findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    try {
+      const orders = await firstValueFrom(
+        this.client.send('findAllOrders', orderPaginationDto),
+      );
+      return orders;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get(':status')
@@ -38,7 +43,7 @@ export class OrdersController {
     @Query() paginationDto: PaginationDto,
   ) {
     try {
-      return this.orderClient.send('findAllOrders', {
+      return this.client.send('findAllOrders', {
         ...paginationDto,
         status: statusDto.status,
       });
@@ -51,7 +56,7 @@ export class OrdersController {
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     try {
       const order = await firstValueFrom(
-        this.orderClient.send('findOneOrder', { id }),
+        this.client.send('findOneOrder', { id }),
       );
       return order;
     } catch (error) {
@@ -65,7 +70,7 @@ export class OrdersController {
     @Body() statusDto: StatusDto,
   ) {
     try {
-      return this.orderClient.send('changeOrderStatus', {
+      return this.client.send('changeOrderStatus', {
         id,
         status: statusDto.status,
       });
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,31 +13,31 @@ import {
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE, PRODUCT_SERVICE } from 'src/config';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productClient: ClientProxy,
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
   ) {}
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productClient.send({ cmd: 'create_product' }, createProductDto);
+    return this.client.send({ cmd: 'create_product' }, createProductDto);
   }
 
   @Get()
   findProducts(@Query() paginationDto: PaginationDto) {
-    return this.productClient.send({ cmd: 'find_all' }, paginationDto);
+    return this.client.send({ cmd: 'find_all' }, paginationDto);
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
     try {
       const product = await firstValueFrom(
-        this.productClient.send({ cmd: 'find_one_product' }, { id }),
+        this.client.send({ cmd: 'find_one_product' }, { id }),
       );
       return product;
     } catch (error) {
@@ -47,7 +47,7 @@ export class ProductsController {
 
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {
-    return this.productClient.send({ cmd: 'delete_product' }, { id });
+    return this.client.send({ cmd: 'delete_product' }, { id });
   }
 
   @Patch(':id')
@@ -55,7 +55,7 @@ export class ProductsController {
     @Param('id', ParseIntPipe) id: string,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productClient
+    return this.client
       .send(
         { cmd: 'update_product' },
         {
